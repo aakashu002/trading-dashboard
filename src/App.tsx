e57245f422117929ef48b037467aa9392609ba83
jonvelophere <jon@velosphere.io>
2025-11-26 15:13:41 +0000

    basic Installation

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import { useState } from "react";
+import "./App.css";
+import LiveTracker from "./Component/LiveTracker";
+
+function App() {
+  return (
+    <div className="relative">
+      <LiveTracker />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Component/LiveTracker.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function LiveTracker() {
+  return <div>LiveTracker</div>;
+}
+
+export default LiveTracker;
